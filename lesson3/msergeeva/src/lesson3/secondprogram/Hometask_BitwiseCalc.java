a553f1dcf610014c19d4c5aac34585e61999c6ea
maria0510 <mariadubil05@gmail.com>
2016-11-13 21:28:37 +0200

    lesson3 (change)-Maria Sergeeva

--- lesson3/msergeeva/src/lesson3/secondprogram/Hometask_BitwiseCalc.java
+++ lesson3/msergeeva/src/lesson3/secondprogram/Hometask_BitwiseCalc.java
@@ -1,44 +1,43 @@
 package lesson3.secondprogram;
 
 class BitwiseCalc{
-   
-   public static void main(String[] args) {
-        String a = "00001111";
-		String c = "00101010";      
-        String b = new String();
+  public static void main(String[] args) {
+    String a = "00001111";
+		String c = "00101010";
+    String b = new String();
 		b = and(a, c);
-        System.out.println("Bitwise operator &: " + b);	
-		
-        String b1 = new String();
-        b1 = or(a, c);
+    System.out.println("Bitwise operator &: " + b);
+
+    String b1 = new String();
+    b1 = or(a, c);
 		System.out.println("Bitwise operator |: " + b1);
-		
+
 		String b2 = new String();
 		b2 = xor(a, c);
 		System.out.println("Bitwise operator ^: " + b2);
-		
+
 		String b3 = new String();
 		b3 = not(c);
 		System.out.println("Bitwise operator ~: " + b3);
-    }
-	
+  }
+
 	public static String and(String binaryLeft, String binaryRight){
-	    int i = Integer.parseInt(binaryRight, 2) & Integer.parseInt(binaryLeft, 2);
+	  int i = Integer.parseInt(binaryRight, 2) & Integer.parseInt(binaryLeft, 2);
 		return  Integer.toBinaryString(i);
 	}
-	
-    public static String or(String binaryLeft, String binaryRight){
-	    int i = Integer.parseInt(binaryRight, 2) | Integer.parseInt(binaryLeft, 2);
+
+  public static String or(String binaryLeft, String binaryRight){
+	  int i = Integer.parseInt(binaryRight, 2) | Integer.parseInt(binaryLeft, 2);
 		return  Integer.toBinaryString(i);
 	}
-	
-    public static String xor(String binaryLeft, String binaryRight){
-	    int i = Integer.parseInt(binaryRight, 2) ^ Integer.parseInt(binaryLeft, 2);
+
+  public static String xor(String binaryLeft, String binaryRight){
+	  int i = Integer.parseInt(binaryRight, 2) ^ Integer.parseInt(binaryLeft, 2);
 		return  Integer.toBinaryString(i);
 	}
-	
-    public static String not(String binary){
-	    int i = ~ Integer.parseInt(binary, 2);
+
+  public static String not(String binary){
+	  int i = ~ Integer.parseInt(binary, 2);
 		return  Integer.toBinaryString(i);
 	}
 }
--- lesson3/msergeeva/src/lesson3/secondprogram/SecondHometask1.java
+++ lesson3/msergeeva/src/lesson3/secondprogram/SecondHometask1.java
@@ -2,10 +2,9 @@ package lesson3.secondprogram;
 
 import java.util.Arrays;
 
-class SecondHometask1{
-   
-   public static void main(String[] args) {
-        int[] a = {1,2,3};
+class SecondHometask1{ 
+  public static void main(String[] args) {
+    int[] a = {1,2,3};
 		int[] b = null;
 		
 		ArraysCopier arr = new ArraysCopier(a);
@@ -22,48 +21,48 @@ class SecondHometask1{
 		b = arr1.fill(8);
 		ArraysCopier arr3 = new ArraysCopier(b);
 		System.out.println("The fill array: {" + arr3 + "}");
-    }
+  }
 }	
 class ArraysCopier{
-    public int[] array = null;
+  public int[] array = null;
 	int[] k = null;
 	
 	public ArraysCopier(int[] array){
-	    this.array = array;
+	  this.array = array;
 	}
 	public String toString() {
-	    String k = " ";
+	  String k = " ";
 		for (int i = 0; i < array.length; i++) {
-		    k += Integer.toString(array[i]) + " ";
+		  k += Integer.toString(array[i]) + " ";
 		}
-	    return k;
+	  return k;
 	}
 	public int[] copyOf(int newLength) {
-	    k = new int[newLength];
+	  k = new int[newLength];
 		int j = 0;
 		if (newLength >= array.length) {
-		   j = array.length;
+		  j = array.length;
 		} else {
-		   j = newLength;
+		  j = newLength;
 		}
 		for(int i = 0; i < j; i++) {
-		   k[i] = array[i];
+		  k[i] = array[i];
 		}
-	   	 return k;
+	   	return k;
 	}
 	public int[] copyOfRange(int from, int to) {
-	    k = new int[to - from];
-	    for(int j = 0; j < (to - from); j++){
-	       k[j] = array[j + from];
-	    }
-	    return k;
+	  k = new int[to - from];
+	  for(int j = 0; j < (to - from); j++){
+	    k[j] = array[j + from];
+	  }
+	  return k;
 	}
 	public int[] fill(int value) {
-	    k = new int[array.length];
-	    for(int j = 0; j < array.length; j++) {
-	       k[j] = value;
-	    }
-	    return k;  
+	  k = new int[array.length];
+	  for(int j = 0; j < array.length; j++) {
+	    k[j] = value;
+	  }
+	  return k;  
 	} 
- }	
+}	
 	
